f2fa14185ccc6afab6b62d526826a99fe7c37a97
Bruna <brunadasilvabz98@gmail.com>
2022-12-13 20:26:46 +0100

    add style archives

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom";
 const CharacterCard = (props) => {
   //console.log(props);
   return (
-    <li>
+    <li className="listElement">
       {/* // <li key={props.character.id}> */}
       <Link to={`/CharacterDetail/${props.character.id}`}>
         <img
-          className=""
+          className="listElement--img"
           src={props.character.image}
           alt={props.character.name}
           title={props.character.name}
         />
-        <h4>{props.character.name}</h4>
-        <p>{props.character.species}</p>
+        <div className="listElement--div">
+          <h4 className="listElement__div--name">{props.character.name}</h4>
+          <p className="listElement__div--text">{props.character.species}</p>
+        </div>
       </Link>
     </li>
   );
